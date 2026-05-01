22da044abdd11128cafe6e51f7221cfc93e648c9
Nicole B. <nikkuru2022@gmail.com>
2026-05-01 16:42:46 -0600

    again

--- app/app.js
+++ app/app.js
@@ -1,3 +1,7 @@
+// Force light appearance (ns-light + Android night mode off) so white/navy CSS wins over system dark mode.
+import Theme from '@nativescript/theme';
+Theme.setMode(Theme.Light);
+
 import { Application } from '@nativescript/core';
 import './app.css';
 
